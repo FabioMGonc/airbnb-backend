134ec4141d6cb8430d90eec0091ab3a33bc17d99
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2025-05-22 00:28:43 -0300

    feat: Criação do userSchema

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from "express";
 import "dotenv/config";
 import { conectDB } from "./config/db.js";
+import User from "./models/User.js";
 
 const { PORT } = process.env;
 const app = express();
+
+app.get('/users', async (req, res) => {
+    conectDB();
+    const userDoc = await User.find()
+    res.json(userDoc)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is runnig at ${PORT}`)
